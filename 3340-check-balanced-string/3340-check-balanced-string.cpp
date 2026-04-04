56baad807aff0ad84594b4882a0f8beb1c9eff69
Vishu18-xyz <pqndey9179@gmail.com>
2026-04-04 23:02:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.45%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int n = num.size();
+        int even = 0;
+        int odd = 0;
+        for(int i = 0; i<n; i++){
+            if(i%2==0){
+              even = even + (num[i] - '0');
+            }
+            else{
+                odd = odd + (num[i] - '0');
+            }
+        }
+
+        return even == odd;
+    }
+};
