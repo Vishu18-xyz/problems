6a230e3c68ac045f5c3b61a619ac633c51fa0f48
Vishu18-xyz <pqndey9179@gmail.com>
2026-04-26 14:23:47 +0530

    Time: 0 ms (100%), Space: 348.6 MB (79.85%) - LeetHub

---
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseEvenLengthGroups(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* curr = head;
+        ListNode* prev = dummy;
+
+        int group = 0;
+
+        while (curr) {
+            int count = 0;
+            ListNode* temp = curr;
+
+            while (temp && count < group) {
+                count++;
+                temp = temp->next;
+            }
+
+            if (count % 2 == 0) {
+                ListNode* node = curr;
+                ListNode* prevNode = temp;
+                int k = count;
+
+                while (k--) {
+                    ListNode* nextNode = node->next;
+                    node->next = prevNode;
+                    prevNode = node;
+                    node = nextNode;
+                }
+
+                ListNode* newTail = curr;
+                prev->next = prevNode;
+
+                prev = newTail;
+                curr = temp;
+            }
+            else {
+                int k = count;
+                while (k--) {
+                    prev = curr;
+                    curr = curr->next;
+                }
+            }
+
+            group++;
+        }
+
+        return dummy->next;
+    }
+};
