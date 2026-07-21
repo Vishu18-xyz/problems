ab32bd97b723d98131e4aaf49191d97de1fcb757
Vishu18-xyz <pqndey9179@gmail.com>
2026-07-21 22:32:27 +0530

    0509-fibonacci-number .cpp 0 ms (100%) 8.3 MB (11.26%) Easy

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int fibonacci(int n , vector<int>& dp){
+    int fib(int n) {
+
+        vector<int> dp(n+1);
+        //return fibonacci(n,dp);
+
         if(n<=1){
             return n;
         }
 
-        if(dp[n] != -1){
-            return dp[n];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        
-        return dp[n] = fibonacci(n-1,dp)+fibonacci(n-2,dp);
-    }
-    int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return fibonacci(n,dp);
+
+        return dp[n];
+
     }
 };
