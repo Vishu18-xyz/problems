d5ccd8653e8f7d6a8af9d9ea7a2d007e105ba758
Vishu18-xyz <pqndey9179@gmail.com>
2026-07-26 07:17:04 +0530

    0198-house-robber .cpp 0 ms (100%) 10.2 MB (79.15%) Medium

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
 
-    int moneyrob(int n, vector<int> nums,vector<int> & dp){
-        
-        if(n==0){
-            return dp[n] = nums[0];
-        }
-
-        if(n==1){
-            return dp[n] = max(nums[0],nums[1]);
-        }
-
-        if(dp[n] != -1){
-            return max(nums[n]+dp[n-2],dp[n-1]);
-        }
-
-
-        return dp[n] = max(nums[n]+moneyrob(n-2,nums,dp),moneyrob(n-1,nums,dp));
-    }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> dp(n,-1);
+        if(n==1){
+            return nums[0];
+        }
 
-        return moneyrob(n-1,nums,dp);
+        if(n==2){
+            return max(nums[0],nums[1]);
+        }
+
+        vector<int> dp(3,0);
+
+        dp[1] = nums[0];
+        dp[2] = max(nums[0],nums[1]);
+
+        for(int i = 2; i<n; i++){
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = max(nums[i]+dp[0],dp[1]);
+        }
+
+        return dp[2];
     }
 };
