e794919677dc72c2335366b99ca38b879ddbd2b3
Vishesh Pandey <pqndey9179@gmail.com>
2026-09-12 14:32:49 +0530

    0279-perfect-squares .cpp 187 ms (17.62%) 162.3 MB (10.23%) Medium

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -20,7 +20,24 @@ public:
 
     int numSquares(int n) {
         int size = sqrt(n);
-        vector<vector<int>> dp(size+1,vector<int>(n+1,-1));
-        return solve(n,size,dp);
+        vector<vector<int>> dp(size+1,vector<int>(n+1,1e6));
+
+        for(int i = 0; i<=size; i++){
+            dp[i][0] = 0;
+        }
+
+        for(int i = 1; i<=size; i++){
+            int square = i*i;
+            for(int j = 1; j<=n; j++){
+                if(j-square>=0){
+                    dp[i][j] = min(dp[i-1][j],1+dp[i][j-square]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[size][n];
     }
 };
