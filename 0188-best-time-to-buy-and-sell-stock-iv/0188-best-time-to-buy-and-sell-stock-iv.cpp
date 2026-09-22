4b179cbdf8135d9947a706d56fbb4e0eac45157c
Vishesh Pandey <pqndey9179@gmail.com>
2026-09-22 21:21:30 +0530

    0188-best-time-to-buy-and-sell-stock-iv .cpp 6 ms (62.84%) 18.3 MB (41.08%) Hard

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int maximumprofit(int index, int buy, int transaction , int n, vector<int> prices,vector<vector<vector<int>>> &dp){
-
-        if(index == n || transaction == 0){
-            return 0;
-        }
-
-        if(dp[index][buy][transaction] != -1){
-            return dp[index][buy][transaction];
-        }
-
-
-        if(buy){
-            return dp[index][buy][transaction] = max(-prices[index]+maximumprofit(index+1,0,transaction,n,prices,dp),maximumprofit(index+1,1,transaction,n,prices,dp));
-        }
-        else{
-            return dp[index][buy][transaction] = max(prices[index]+maximumprofit(index+1,1,transaction-1,n,prices,dp),maximumprofit(index+1,0,transaction,n,prices,dp));
-        }
-    }
     int maxProfit(int k,vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
 
-        return maximumprofit(0,1,k,n,prices,dp);
+          for(int i = n-1; i>=0;i--){
+            for(int j =0; j<=1; j++){
+                for(int l = 1; l<=k; l++){
+                    if(j==1){
+                        dp[i][j][l] = max(-prices[i]+dp[i+1][0][l],dp[i+1][1][l]);
+                    }
+                    else{
+                        dp[i][j][l] = max(prices[i]+dp[i+1][1][l-1],dp[i+1][0][l]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][k];
     }
 };
